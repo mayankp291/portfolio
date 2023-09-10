cceeafd9d48fbabf59790c793fc9a18e94840676
Mayank Panjiyara <revflash@LAPTOP-8712783.localdomain>
2023-09-11 01:40:30 +0800

    Updates

---
+++ static/js/3.0bb47f47.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[3],{58:function(t,o,i){"use strict";i.r(o),o.default=i.p+"static/media/aboutMySelf.english.6972b34e.md"}}]);
+//# sourceMappingURL=3.0bb47f47.chunk.js.map
---
+++ static/js/4.58a72db7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[4],{57:function(o,t,a){"use strict";a.r(t),t.default=a.p+"static/media/aboutMySelf.korean.3a60b0be.md"}}]);
+//# sourceMappingURL=4.58a72db7.chunk.js.map
